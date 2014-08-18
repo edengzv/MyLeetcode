bece75bfa64883313decfde47d6ba358fd527474
kantian <blackknight731@gmail.com>
2014-08-18 17:16:01 +0800

    you have solved 7/151

---
+++ Linked_List_Cycle.cc
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode *a = head;
+        ListNode *b = head;
+        while(a && b && b->next != NULL){
+            a = a->next;
+            b = b->next->next;
+            if(a == b){
+                return true;
+            }
+        }
+        return false;
+    }
+};
