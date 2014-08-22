db60de6617950eb56192ba310b4d53eb7f68eead
kantian <blackknight731@gmail.com>
2014-08-22 10:19:38 +0800

    you have solved 9/151

---
+++ Remove_Duplicates_from_Sorted_Array.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int removeDuplicates(int A[], int n) {
+        int v = 0;
+        if(n == 0) return 0;
+        for(int i=1;i<n;i++){
+            if(A[v] != A[i]){
+                A[++v] = A[i];
+            }
+        }
+        return v+1;
+    }
+};
